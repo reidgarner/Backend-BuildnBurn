daadf18d68e58b5f8c6d7876849dfbf2e7d4fc25
reidgarner <reidbgarner@gmail.com>
2019-01-02 11:24:35 -0700

    added movies routes file

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const port = process.env.PORT || 3003
+const movies = require('./routes/movies')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use('/movies', movies)
 
 app.listen(port, () => {
     console.log(`running on port: ${port}`)
